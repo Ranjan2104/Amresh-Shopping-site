8804ab6c2b082e9eb1d5e4a43f633930c30dbcb1
Ranjan2104 <ranjan.amresh100@gmail.com>
2023-08-06 12:17:47 +0530

    remove css property

--- src/Components/Adds.jsx
+++ src/Components/Adds.jsx
@@ -5,10 +5,11 @@ const AdCard = () => {
   return (
     <div>
       <Box>
-        <img className='adRight' src='https://img.pikbest.com/png-images/20191030/cartoon-cute-women-shopping-shopping-dynamic-gif-elements_2662094.png!bw700' />
+        <img src='https://img.pikbest.com/png-images/20191030/cartoon-cute-women-shopping-shopping-dynamic-gif-elements_2662094.png!bw700' 
+        style={{ width: '255px', height: '17rem' }}/>
       </Box>
       <Box sx={{ paddingTop:'15px' }}>
-        <img className='adRight'  src='https://cdn.dribbble.com/users/1948198/screenshots/4377223/dribble.gif'/>
+        <img src='https://cdn.dribbble.com/users/1948198/screenshots/4377223/dribble.gif' style={{ width: '255px', height: '17rem' }}/>
       </Box>
     </div>
   )
